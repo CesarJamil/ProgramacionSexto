973fecf161c66fd9bf520adf370f62a1ea965787
cesar <cesar@localhost.localdomain>
2017-11-08 08:29:45 -0500

    tercera version

--- VirtualEnvs/SitioWeb/Sitio/Pagina/urls.py
+++ VirtualEnvs/SitioWeb/Sitio/Pagina/urls.py
@@ -5,4 +5,10 @@ urlpatterns = [
 	url(r'^$', views.mostrar_inicio),
 	url(r'^ventana2/$', views.widgets),
 	url(r'^ventana3/$', views.calendar),
+	url(r'^dashboardv2/$', views.dashboardv2),
+	url(r'^topNav/$', views.topNav),
+	url(r'^boxed/$', views.boxed),
+	url(r'^fixed/$', views.fixed),
+	url(r'^collapsedSidebar/$', views.collapsedSidebar),
+	url(r'^chartjs/$', views.chartjs),
 ]
--- VirtualEnvs/SitioWeb/Sitio/Pagina/views.py
+++ VirtualEnvs/SitioWeb/Sitio/Pagina/views.py
@@ -9,4 +9,22 @@ def widgets(request):
 	return render(request, 'Pagina/widgets.html', {})
 
 def calendar(request):
-	return render(request, 'Pagina/calendar.html')
+	return render(request, 'Pagina/calendar.html', {})
+
+def dashboardv2(request):
+	return render (request, 'Pagina/dashboard2.html', {})
+
+def topNav(request):
+	return render (request, 'Pagina/topNav.html', {})
+
+def boxed(request):
+	return render (request, 'Pagina/boxed.html', {})
+
+def fixed(request):
+	return render (request, 'Pagina/fixed.html', {})
+
+def collapsedSidebar(request):
+	return render (request, 'Pagina/collapsedSidebar.html', {})
+
+def chartjs(request):
+	return render (request, 'Pagina/chartjs.html', {})
